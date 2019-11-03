5c290c8d4d2c6a3402ba19785a2642755dd0fd84
Aaron <at2fb@virginia.edu>
2019-11-03 18:45:04 -0500

    landing page

---
+++ final-project/src/Components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { styled } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+
+export default function(){
+    return(
+        <div>
+            <h1>WELCOME TO THE LANDING PAGE</h1>
+            <Button>Sign Up</Button>
+            <Button>Login</Button>
+        </div>
+    )
+}
