c7132bc2d4ed6b6d08a271647c774ccf5e095957
Arjun Kumar <arjunkumar@Arjuns-MacBook-Pro.local>
2019-11-10 18:19:48 -0500

    Added some css

--- final-project/src/Components/Home.js
+++ final-project/src/Components/Home.js
@@ -2,13 +2,16 @@ import React from 'react';
 import { styled } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import { fontSize } from '@material-ui/system';
+import './CSS Files/LandingPage.css';
 
 export default function(){
     return(
-        <div style={{margin: 0,}}>
-            <h1 style={{fontSize: "50px",}}>WELCOME TO THE LANDING PAGE</h1>
-            <Button>Sign Up</Button>
-            <Button>Login</Button>
+        <div class="header">
+            <p class = "title">WELCOME TO THE LANDING PAGE</p>
+            <div class="buttons">
+                <Button>Sign Up</Button>
+                <Button>Login</Button>
+            </div>
         </div>
     )
 }
