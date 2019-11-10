11cccbbad690f7660878ec38633269b5b9977c7b
Aaron <at2fb@virginia.edu>
2019-11-10 17:29:23 -0500

    update

--- final-project/src/Components/Home.js
+++ final-project/src/Components/Home.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { styled } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
+import { fontSize } from '@material-ui/system';
 
 export default function(){
     return(
-        <div>
-            <h1>WELCOME TO THE LANDING PAGE</h1>
+        <div style={{margin: 0,}}>
+            <h1 style={{fontSize: "50px",}}>WELCOME TO THE LANDING PAGE</h1>
             <Button>Sign Up</Button>
             <Button>Login</Button>
         </div>
